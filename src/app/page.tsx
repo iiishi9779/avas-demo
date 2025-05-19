a8bf5582e8be4ee3aba409a0027839f5c927e81e
iiishi9779 <ezimadzez@gmail.com>
2025-05-19 09:58:43 +0500

    app layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,63 @@
 import Image from "next/image";
+import SuggestionItem from "@/components/suggestion-item";
+import LocationItem from "@/components/location-item";
+import NavigationBar from "@/components/navigation-bar";
+import {
+  MagnifyingGlassIcon,
+  ClockIcon,
+  ChevronRightIcon,
+} from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    <div className="bg-gray-50 grid grid-rows-[20px_1fr_20px] min-h-screen pb-20 font-[family-name:var(--font-geist-sans)]">
+      <main className="flex px-8 flex-col gap-[32px] row-start-2">
+        <header className="flex gap-4">
+          <Image
+            className="dark:invert"
+            src="/next.svg"
+            alt="Next.js logo"
+            width={80}
+            height={80}
+            priority
+          />
+          <div className="text-3xl font-bold">Avas App</div>
+        </header>
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
+        <div className="flex gap-4 bg-gray-100 rounded-full p-4 items-center">
+          <MagnifyingGlassIcon className="size-8 text-gray-500" />
+          <input
+            className="flex-1 placeholder-gray-500 text-xl font-medium"
+            type="text"
+            placeholder="Where to?"
+          />
+          <div className="px-2 py-1 flex items-center gap-1 bg-white rounded-full">
+            <ClockIcon className="size-5 text-gray-800" />
+            <span className="font-medium">Now</span>
+          </div>
+          <ChevronRightIcon className="size-5 text-gray-800" />
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <LocationItem title="Work" subtitle="1455 Market St" />
+          <LocationItem title="Home" subtitle="903 Sunrose Terr" />
+        </div>
+
+        <div className="flex justify-between">
+          <h2 className="text-xl font-medium text-gray-700">Suggestions</h2>
+          <div className="text-gray-700">See all</div>
+        </div>
+        <div className="flex gap-4">
+          <SuggestionItem title="ride" imgSrc="/globe.svg" />
+          <SuggestionItem title="package" imgSrc="/globe.svg" />
+          <SuggestionItem title="reserve" imgSrc="/globe.svg" />
+          <SuggestionItem title="rent" imgSrc="/globe.svg" />
+        </div>
+
+        <h2 className="text-xl font-medium text-gray-700">
+          Ways to plan with Avas
+        </h2>
+        {/* <div className="flex gap-4 items-center flex-col sm:flex-row">
           <a
             className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
             href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
@@ -49,55 +81,9 @@ export default function Home() {
           >
             Read our docs
           </a>
-        </div>
+        </div> */}
       </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      <NavigationBar />
     </div>
   );
 }
